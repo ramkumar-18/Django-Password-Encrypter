3d1441bcf3d372c111ba6f13c410f8bf7069501d
ramkumar-18 <ramkumar.b2022ai-ds@sece.ac.in>
2024-11-21 21:14:55 +0530

    Added env

--- Cypher/settings.py
+++ Cypher/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-__tdx(n72-m)il@)kln5)!wdsden#vmj8081*0!qf*=bvn2gi#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
